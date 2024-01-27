69516a4ceb979e1266b4c7538dac6dd0e24e918a
Matías Beraldo Fernández <matias.beraldo.work@gmail.com>
2024-01-27 23:22:24 +0100

    Refactored the baseUrl so it's not necessary to set it over and over again. Injecting GitHub Action GATEWAY_URL dynamically

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const GATEWAY_URL = process.env.GATEWAY_URL;
+const ACCOUNT_ID = process.env.ACCOUNT_ID || "sdks.near";
+const BASE_URL = `${GATEWAY_URL}/${ACCOUNT_ID}/widget/`;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,7 +23,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/sdks.utils.cryptojs.spec.ts
+++ tests/sdks.utils.cryptojs.spec.ts
@@ -1,13 +1,8 @@
 import { test, expect, Page } from '@playwright/test';
-import 'dotenv/config';
-
-const GATEWAY_URL = process.env.GITHUB_ACTIONS ? "http://127.0.0.1:4040" : process.env.GATEWAY_URL;
-const ACCOUNT_ID = process.env.ACCOUNT_ID || "sdks.near";
-
-const WIDGET_URL = `${GATEWAY_URL}/${ACCOUNT_ID}/widget/Examples.CryptoJS`;
+const WIDGET = `Examples.CryptoJS`;
 
 test('it hashes text', async ({ page }: { page: Page }) => {
-  await page.goto(WIDGET_URL);
+  await page.goto(WIDGET);
 
   await hashText(page, "test");
 
@@ -15,7 +10,7 @@ test('it hashes text', async ({ page }: { page: Page }) => {
 });
 
 test('it hashes text multiple times', async ({ page }: { page: Page }) => {
-  await page.goto(WIDGET_URL);
+  await page.goto(WIDGET);
 
   await hashText(page, "test");
   await expect(page.getByText('098f6bcd4621d373cade4e832627b4f6')).toBeVisible();
